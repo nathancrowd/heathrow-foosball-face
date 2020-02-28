c557b1f86e57dd5b4d8ae1a3b6847646cb6fdf36
Nathan <nathan@thisiscrowd.com>
2020-02-28 16:16:33 +0000

    Lay out some face filter options

--- face-filter/src/js/essential/capture-faces.js
+++ face-filter/src/js/essential/capture-faces.js
@@ -13,6 +13,7 @@ export default class FaceCapture {
     }
 
     async load(res) {
+        console.log('FaceCapture: Loading models...');
         await faceapi.loadSsdMobilenetv1Model(this.MODEL_URL);
         await faceapi.loadFaceLandmarkModel(this.MODEL_URL);
         await faceapi.loadFaceRecognitionModel(this.MODEL_URL);
@@ -22,18 +23,27 @@ export default class FaceCapture {
     }
 
     async detect(res) {
+        console.log('FaceCapture: Reading Faces...');
         let detections = await faceapi.detectAllFaces(this.videoEl);
         detections = faceapi.resizeResults(detections, {width: this.videoEl.width, height: this.videoEl.height});
         this.detections = detections;
+        console.log('FaceCapture: Faces Captured');
         res(this);
     }
 
-    returnCanvases() {
+    returnCanvases(cb) {
         let canvases = [];
-        this.detections.forEach(async d => {
-            canvases.push(await faceapi.extractFaces(this.videoEl, d));
+        new Promise((res,rej) => {
+            this.detections.forEach(async (d,i) => {
+                let c = await faceapi.extractFaces(this.videoEl, [d]);
+                canvases.push(c);
+                if (i == this.detections.length - 1) {
+                    res();
+                }
+            });
+        }).then(() => {
+            cb(canvases);
         });
 
-        return canvases;
     }
 }
--- face-filter/src/js/essential/main.js
+++ face-filter/src/js/essential/main.js
@@ -1,14 +1,19 @@
 import captureVideo from './video-capture';
 import FaceCapture from './capture-faces';
+import {createScene, addFace} from './scene';
 
 document.addEventListener('DOMContentLoaded', () => {
 
     let videoEl = captureVideo();
 
+    createScene();
+
     videoEl.addEventListener('loadeddata', () => {
         setTimeout(() => {
-            new FaceCapture(videoEl).then((faces) => {
-                console.log(faces.returnCanvases());
+            new FaceCapture(videoEl).then((faces) => {                
+                faces.returnCanvases((canvases) => {
+                    addFace(canvases[0][0]);
+                });
             });
         }, 5000);
     }, false);
